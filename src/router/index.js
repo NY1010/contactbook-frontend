7aa9f15d107bcdc495a9022ae0535504df8d9963
Ngoc Yen <yenb2103576@student.ctu.edu.vn>
2023-08-24 16:16:53 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -7,11 +7,22 @@ const routes = [
         name: "contactbook",
         component: ContactBook,
     },
+
+    {
+        path: "/:pathMatch(.*)*",
+        name: "notfound",
+        component: () => import("@/views/NotFound.vue"),
+    },
 ];
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes,
+
+
+    path: "/:pathMatch(.*)*",
+    name: "notfound",
+    component: () => import("@/views/NotFound.vue"),
 });
 
 export default router;
